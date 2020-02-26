0d671664b8b45305e11a809ff92cc183212a4265
Mohammed Nouman PK <mnoumanp>
2020-02-26 15:43:49 +0530

    first commit

---
+++ src/main/java/com/example/docker/dockerizeSPA/DockerizeSpaApplication.java
@@ -0,0 +1,15 @@
+//package com.example.docker.dockerizeSPA;
+package com.example.docker.dockerizeSPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerizeSpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerizeSpaApplication.class, args);
+		System.out.println("hello World");
+	}
+
+}
---
+++ src/test/java/com/example/docker/dockerizeSPA/DockerizeSpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.docker.dockerizeSPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerizeSpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
